03bce7ae4cfcf40312ae9ad17a1ce93c9c5e7b35
Ursu-Ioana-Bianca <ioana.ursu@info.uaic.ro>
2021-04-10 20:05:06 +0300

    LeHS

---
+++ XAMPP/settings.js
@@ -0,0 +1,89 @@
+var nyan = document.getElementById('nyan');
+var nyanBtn = document.getElementById('nyan-btn');
+
+function playPause(song){
+   if (song.paused && song.currentTime >= 0 && !song.ended) {
+      song.play();
+   } else {
+      song.pause();
+   }
+}
+
+function reset(btn, song){
+   if(btn.classList.contains('playing')){
+      btn.classList.toggle('playing');
+   }
+   song.pause();
+   song.currentTime = 0;
+}
+
+function progress(btn, song){
+   setTimeout(function(){
+      var end = song.duration; 
+      var current = song.currentTime;
+      var percent = current/(end/100);
+      //check if song is at the end
+      if(current==end){
+         reset(btn, song);
+      }
+      //set inset box shadow
+      btn.style.boxShadow = "inset " + btn.offsetWidth * (percent/100) + "px 0px 0px 0px rgba(0,0,0,0.125)"
+      //call function again
+      progress(btn, song);     
+   }, 1000);
+}
+
+nyanBtn.addEventListener('click', function(){
+   nyanBtn.classList.toggle('playing');
+   playPause(nyan);
+   progress(nyanBtn, nyan);
+});
+
+
+
+
+var french = document.getElementById('fr_click'),
+    english = document.getElementById('en_click'),
+    fr_txt = document.querySelectorAll('#fr'),
+    en_txt = document.querySelectorAll('#en'),
+    nb_fr = fr_txt.length,
+    nb_en = en_txt.length;
+
+french.addEventListener('click', function() {
+    langue(french,english);
+}, false);
+
+english.addEventListener('click', function() {
+    langue(english,french);
+}, false);
+
+function langue(langueOn,langueOff){
+    if (!langueOn.classList.contains('current_lang')) {
+        langueOn.classList.toggle('current_lang');
+        langueOff.classList.toggle('current_lang');
+    }
+    if(langueOn.innerHTML == 'Fr'){
+        afficher(fr_txt, nb_fr);
+        cacher(en_txt, nb_en);
+    }
+    else if(langueOn.innerHTML == 'En'){
+        afficher(en_txt, nb_en);
+        cacher(fr_txt, nb_fr);
+    }
+}
+
+function afficher(txt,nb){
+    for(var i=0; i < nb; i++){
+        txt[i].style.display = 'block';
+    }
+}
+function cacher(txt,nb){
+    for(var i=0; i < nb; i++){
+        txt[i].style.display = 'none';
+    }
+}
+function init(){
+    langue(french,english);
+}
+init();
+
